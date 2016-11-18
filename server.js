f29f260be067e97be43fef0a79c41a2d473820bd
bryanlian <bryan.lian.2014@sis.smu.edu.sg>
2016-11-18 23:05:19 +0800

    heroku 2

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+app = express();
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/app'));
-
-// set the home page route
+app.use(express.static(__dirname));
 app.get('/', function(req, res) {
-
-    // make sure index is in the right directory. In this case /app/index.html
-    res.render('index');
+    res.sendfile('index.html', {root: __dirname })
 });
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+var server = app.listen(process.env.PORT || 80);
